41deadfbcbae52b54104fdd21d499228160bba99
Jose Hidalgo <jhidalgo@innova-techs.com>
2015-02-24 23:44:13 -0600

    Arreglamos el Zoom y colocamos 2 puntos gps.

--- app/src/main/java/tallerescr/innova_techs/com/tallerescr/MapsActivity.java
+++ app/src/main/java/tallerescr/innova_techs/com/tallerescr/MapsActivity.java
@@ -1,16 +1,28 @@
 package tallerescr.innova_techs.com.tallerescr;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private LatLng taller1 = new LatLng(9.5206184,-84.3297243);
+    private LatLng taller2 = new LatLng(9.904397,-84.099178);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +56,7 @@ public class MapsActivity extends FragmentActivity {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+            mMap= ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
@@ -60,7 +72,47 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Hola"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(-84.3297243, 9.5206184)).title("Hola"));
+        // TODO Auto-generated method stub
+        // Hide the zoom controls as the button panel will cover it.
+        //mUiSettings = mMap.getUiSettings();
+        // Enables/disables zoom gestures (i.e., double tap, pinch & stretch).
+        mMap.getUiSettings().setZoomGesturesEnabled(false);
+// Enables/disables scroll gestures (i.e. panning the map).
+        mMap.getUiSettings().setScrollGesturesEnabled(false);
+        // Enables/disables the compass (icon in the top left that indicates the orientation of the
+        // map).
+        mMap.getUiSettings().setCompassEnabled(false);
+        // Add lots of markers to the map.
+        addMarkersToMap();
+
+        // Pan to see all markers in view.
+        // Cannot zoom to bounds until the map has a size.
+        final View mapView = getSupportFragmentManager().findFragmentById(R.id.map).getView();
+        if (mapView.getViewTreeObserver().isAlive()) {
+            mapView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @SuppressLint("NewApi") // We check which build version we are using.
+                @Override
+                public void onGlobalLayout() {
+                    LatLngBounds bounds = new LatLngBounds.Builder()
+                            .include(taller1)
+                            .include(taller2)
+                            .build();
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                        mapView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                    } else {
+                        mapView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                    }
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
+                }
+            });
+        }
+
+
+    }
+
+    private void addMarkersToMap()
+    {
+        mMap.addMarker(new MarkerOptions().position(taller1).title("Hola"));
+        mMap.addMarker(new MarkerOptions().position(taller2).title("Hola"));
     }
 }
